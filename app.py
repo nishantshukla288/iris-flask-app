81bab3b1d71ee4958d876f2b9c047b4e1b683732
nishantshukla288 <hinishuuu@gmail.com>
2025-07-20 21:58:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.preprocessing import LabelEncoder
+import os
 
 app = Flask(__name__)
 
@@ -33,4 +34,6 @@ def index():
     return render_template("index.html", prediction=prediction)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # ✅ Use PORT from environment for deployment
+    app.run(host="0.0.0.0", port=port)
+
